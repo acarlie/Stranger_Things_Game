6cc5fdb1f16dee5ca2c53ca37145a3ff9c2774db
acarlie <ameliacarlie@gmail.com>
2019-07-16 19:15:05 -0400

    Added select buttons and click events on buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
     var game = {
+        defend: false,
         players: [
             {name: "Eleven", desc: "", hawkins: true, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
             {name: "Jim Hopper", desc: "", hawkins: true, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
@@ -10,8 +11,7 @@ $( document ).ready(function() {
         ],
     }
 
-
-
+    window.setTimeout(function(){ $('#loader').addClass('hidden'); }, 1000);
 
     $('.text-stranger').each(function(){
         $(this).attr('data-content', this.textContent);
@@ -20,7 +20,9 @@ $( document ).ready(function() {
 
     $.each(game.players, function(i){
 
-        var character = $('<div>').addClass('player').text(game.players[i].name).attr('data-select', 'false');
+        var character = $('<div>').addClass('player').text(game.players[i].name).attr('data-select', 'false').attr('data-char', i).attr('data-hawkins', game.players[i].hawkins);
+        var selectButton = $('<button>').addClass('btn btn-select btn-hidden').text('Select');
+        character.append(selectButton);
 
         if(game.players[i].hawkins){
             $('#hawkins').append(character);
@@ -30,13 +32,19 @@ $( document ).ready(function() {
         
     });
 
-    $('.player').on("click", function(){
-        $('.player').attr('data-select', 'false');
-        $(this).attr('data-select', 'true'); //use .data() to retrieve later
-        console.log('clicked' + this);
+    $('.player').on('click', function(){
+        $('.btn-select').addClass('btn-hidden');
+        $('.player').attr('data-select', 'false').removeClass('player-chosen');
+        $(this).attr('data-select', 'true').addClass('player-chosen'); //use .data() to retrieve later  
+        $(this).find('button').removeClass('btn-hidden');
     });
 
-    window.setTimeout(function(){ $('#loader').addClass('hidden'); }, 1000);
+    $('.player').on('click', 'button', function(){
+        console.log('clicked');
+    });
+
+
+
     
 
 
