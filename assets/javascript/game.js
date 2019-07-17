f849691ac6b49406a89dcc03acc2a7f5e423ed6c
acarlie <ameliacarlie@gmail.com>
2019-07-17 11:09:38 -0400

    Added random enemy selection

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,19 @@
 $( document ).ready(function() {
     var game = {
-        defend: false,
+        gamePlay: false,
+        character: "",
+        characterSide: "",
+        enemies: [],
+        enemyCount: 0,
+        defeatedEnemies: 0,
+        currentEnemy: "",
         players: [
-            {name: "Eleven", desc: "", hawkins: true, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
-            {name: "Jim Hopper", desc: "", hawkins: true, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
-            {name: "Jancy", desc: "", hawkins: true, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
-            {name: "The Mind Flayer", desc: "", hawkins: false, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
-            {name: "Demogorgon", desc: "", hawkins: false, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
-            {name: "Billy", desc: "", hawkins: false, hp: 100, attack: 100, counterAttack: 100, basePower: 100}
+            {name: "Eleven", num: 1, desc: "", hawkins: true, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
+            {name: "Jim Hopper", num: 2, desc: "", hawkins: true, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
+            {name: "Jancy", num: 3, desc: "", hawkins: true, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
+            {name: "The Mind Flayer", num: 4, desc: "", hawkins: false, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
+            {name: "Demogorgon", num: 5, desc: "", hawkins: false, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
+            {name: "Billy", num: 6, desc: "", hawkins: false, hp: 100, attack: 100, counterAttack: 100, basePower: 100}
         ],
     }
 
@@ -20,7 +26,7 @@ $( document ).ready(function() {
 
     $.each(game.players, function(i){
 
-        var character = $('<div>').addClass('player').text(game.players[i].name).attr('data-select', 'false').attr('data-char', i).attr('data-hawkins', game.players[i].hawkins);
+        var character = $('<div>').addClass('player').text(game.players[i].name).attr('data-select', 'false').attr('data-index', i).attr('data-num', game.players[i].num).attr('data-hawkins', game.players[i].hawkins);
         var selectButton = $('<button>').addClass('btn btn-select btn-hidden').text('Select');
         character.append(selectButton);
 
@@ -40,7 +46,34 @@ $( document ).ready(function() {
     });
 
     $('.player').on('click', 'button', function(){
-        console.log('clicked');
+
+        var playerIndex = $(this).parent().attr('data-index');
+
+        game.gamePlay = true;
+        game.character = game.players[playerIndex];
+        game.characterSide = game.players[playerIndex].hawkins;
+        
+
+        game.players.forEach(function(i){
+            if(game.characterSide !== i.hawkins){
+                game.enemies.push(i);
+                game.enemyCount++;
+            }     
+        });
+
+        var random = Math.floor(Math.random()*game.enemies.length);
+        game.currentEnemy = game.enemies[random];
+        game.enemies.splice(random, 1);
+        var enemyNum = game.currentEnemy.num;
+        console.log($(`.player[data-num=${enemyNum}]`));
+        $(`.player[data-num=${enemyNum}]`).addClass('Hello');
+    
+
+
+       
+
+
+
     });
 
 
