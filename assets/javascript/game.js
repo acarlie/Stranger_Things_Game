8539083aff273bbae30547f75af96d8d9b652b62
acarlie <ameliacarlie@gmail.com>
2019-07-18 14:33:54 -0400

    Added dueling method

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,13 +8,54 @@ $( document ).ready(function() {
         defeatedEnemies: 0,
         currentEnemy: "",
         players: [
-            {name: "Eleven", num: 1, desc: "", hawkins: true, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
-            {name: "Jim Hopper", num: 2, desc: "", hawkins: true, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
-            {name: "Jancy", num: 3, desc: "", hawkins: true, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
-            {name: "The Mind Flayer", num: 4, desc: "", hawkins: false, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
-            {name: "Demogorgon", num: 5, desc: "", hawkins: false, hp: 100, attack: 100, counterAttack: 100, basePower: 100},
-            {name: "Billy", num: 6, desc: "", hawkins: false, hp: 100, attack: 100, counterAttack: 100, basePower: 100}
+            {num: 1, hawkins: true, hp: 60, attack: 50, defense: 25, counterAttack: 100, basePower: 100, name: "Eleven", desc: ""},
+            {num: 2, hawkins: true, hp: 80, attack: 25, defense: 20, counterAttack: 100, basePower: 100, name: "Jim Hopper", desc: ""},
+            {num: 3, hawkins: true, hp: 80, attack: 30, defense: 15, counterAttack: 100, basePower: 100, name: "Jancy", desc: "A.K.A. Jonathan Buyers and Nancy Wheeler"},
+            {num: 4, hawkins: false, hp: 100, attack: 40, defense: 15, counterAttack: 100, basePower: 100, name: "The Mind Flayer", desc: ""},
+            {num: 5, hawkins: false, hp: 90, attack: 35, defense: 15, counterAttack: 100, basePower: 100, name: "Demogorgon", desc: ""},
+            {num: 6, hawkins: false, hp: 60, attack: 35, defense: 10, counterAttack: 100, basePower: 100, name: "Billy", desc: ""}
         ],
+        isAlive(player){
+            //returns if player is alive.
+            return Boolean(player.hp > 0);
+        },
+        duel(player1, player2){
+            // That is, Player 1 attacks Player 2. Then Player 2 attacks Player 1
+            // A Player's damage is defined as their "attack" value minus their opponent's "defense" value. 
+            // The damage is then subtracted from the defender's health value.
+            // While players are alive continue duel.
+            // Is alive boolean checks if player is alive
+
+            //Math.max can be used to prevent negative numbers - Math.max(0, possible damage);
+            // if(this.isAlive(player1) && this.isAlive(player2))
+            if (this.isAlive(player1) && this.isAlive(player2)){
+                player1.hp = Math.max(0, player1.hp - Math.max(0, player2.attack - player1.defense));
+                player2.hp = Math.max(0, player2.hp - Math.max(0, player1.attack - player2.defense));
+
+                // console.log(player1.name + player1.hp);
+                // console.log(player2.name + player2.hp);
+
+                var p1 = player1.num;
+                var p2 = player2.num;
+
+                $('#hp'+ p1).text(player1.hp);
+                $('#hp'+ p2).text(player2.hp);
+
+                if(this.isAlive(player1) && !this.isAlive(player2)){
+                    console.log(player1.name + ' won');
+                } else if (this.isAlive(player2) && !this.isAlive(player1)){
+                    console.log(player2.name + ' won');
+                } else if (!this.isAlive(player1) && !this.isAlive(player2)){
+                    console.log('draw');
+                }
+
+            } 
+
+         
+      
+                
+                
+        },
         appendEl(obj, element){
             if(obj.hawkins){
                 $('#hawkins').append(element);
@@ -23,8 +64,8 @@ $( document ).ready(function() {
             }
         },
         characterCard(obj){
-            var card = $('<div>').text(obj.name).addClass('player-game');
-            var hp = $('<p>').text(obj.hp);
+            var card = $('<div>').text(obj.name).addClass('player player-game').attr('data-num', obj.num);
+            var hp = $('<p>').text(obj.hp).attr('id', 'hp' + obj.num);
             card.append(hp);
 
             if(obj === this.character){
@@ -96,37 +137,22 @@ $( document ).ready(function() {
 
                 var playerIndex = $(this).attr('data-index');
                 game.init(playerIndex);
-                
-                // $( '.player-select' ).off('click');
-                
+                                
             } 
             
           console.log(event.target);
     }
 
-    // function playerAttackHandler( event ){
-    //     var target = $( event.target );
-    //     if ( target.is( '#attack' ) && game.gamePlay ) {
-    //         console.log('Attack!');
-    //     }
-
-    //     console.log( event.target );
-    // }
 
     //player select button
     $( '.player-select' ).on('click', playerSelectHandler ).find( "span, button" ).hide();
 
-    // //attack button
-    // $( '.btn-attack' ).on('click', function(){
-    
-    // });
-
-
-    // $( '.player-game' ).click( playerAttackHandler );
-    $('#hawkins, #upside-down').on('click', '#attack', function(){
-        
-        console.log('attack');
+    //attack button
+    $('#container').on('click', '#attack', function(){
+        game.duel(game.character, game.currentEnemy);
     });
+
+
 
 
    
